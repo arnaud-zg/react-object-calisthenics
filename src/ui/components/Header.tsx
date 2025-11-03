67e2c43c9598d743606c78f084e076b014112867
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2025-11-03 23:44:47 +0100

    chore: add link to github project

--- src/ui/components/Header.tsx
+++ src/ui/components/Header.tsx
@@ -1,28 +1,26 @@
+import { GitHubLogoIcon } from "@radix-ui/react-icons";
 import { Link } from "@tanstack/react-router";
 
 export default function Header() {
   return (
-    <header className="px-10 py-2 flex gap-4 bg-white text-black justify-between h-10 shadow-sm">
+    <header className="px-10 py-2 flex gap-4 bg-white text-black justify-between h-10 shadow-sm items-center">
       <nav className="flex flex-row gap-4 font-semibold">
-        <Link
-          to="/"
-          activeProps={{
-            className: "underline",
-          }}
-          className="text-[#282c34]"
-        >
+        <Link to="/" className="text-[#282c34]">
           Home
         </Link>
-        <Link
-          to="/shopping-cart"
-          activeProps={{
-            className: "underline",
-          }}
-          className="text-[#282c34]"
-        >
+        <Link to="/shopping-cart" className="text-[#282c34]">
           Shopping Cart
         </Link>
       </nav>
+
+      <a
+        href="https://github.com/arnaud-zg/react-object-calisthenics"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-[#282c34] hover:text-gray-700"
+      >
+        <GitHubLogoIcon className="w-6 h-6" />
+      </a>
     </header>
   );
 }
